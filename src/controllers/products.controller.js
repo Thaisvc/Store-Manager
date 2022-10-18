2c226163da967a57a0d6dee1c950130b86340348
thais viana <thaisviana081@outlook.com>
2022-10-17 21:09:41 -0300

    10 - Crie endpoint para atualizar um produto  😵 FRUTA Q PARTIUU

--- src/controllers/products.controller.js
+++ src/controllers/products.controller.js
@@ -26,10 +26,14 @@ const ProductUpdate = async (req, res) => {
   const { id } = req.params;
   const newObj = { name, id };
 
-  const retorno = await productsService.productUpdate(newObj); // RETORNO É = {TYPE, MENSSAGE} DESTRUTURADO VINDO DE productUpdate
+  const { type, message } = await productsService.productUpdate(newObj); // RETORNO É = {TYPE, MENSSAGE} DESTRUTURADO VINDO DE productUpdate
+  // console.log(retorno);
+  if (type) return res.status(errorMap.mapError(type)).json({ message });
+  return res.status(200).json(message);
+  /* const retorno = await productsService.productUpdate(newObj); // RETORNO É = {TYPE, MENSSAGE} DESTRUTURADO VINDO DE productUpdate
   // console.log(retorno);
   if (retorno.type) return res.status(errorMap.mapError(retorno.type)).json(retorno.message);
-  return res.status(200).json(retorno.message);
+  return res.status(200).json(retorno.message); RETORNA MENSSAGEM DE ERRO COMO STRING DANDO ERRO NO TESTE */
 };
 module.exports = {
   allProducts,
