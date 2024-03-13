b4ad80ae0a84a270d994a393122ff0c1bdd74ef8
thais viana <thaisviana081@outlook.com>
2024-03-13 20:31:25 -0300

    ajustes porta

--- src/server.js
+++ src/server.js
@@ -2,7 +2,18 @@ const app = require('./app');
 require('dotenv').config();
 
 // não altere esse arquivo, essa estrutura é necessária para à avaliação do projeto
+// erro porta undefined resolvi com -> const port = process.env.PORT || 3000; // Usando a porta 3000 por padrão se a variável de ambiente não estiver definida
+// app.listen(port, () => {
+    // console.log(`Servidor está rodando na porta ${port}`);
+// });
 
-app.listen(process.env.PORT, () => {
-  console.log(`Escutando na porta ${process.env.PORT}`);
+// codigo antigo
+
+// app.listen(process.env.PORT || 3000, () => {
+  // console.log(`Escutando na porta ${process.env.PORT}`);
+// });
+
+const port = process.env.PORT || 3000; 
+app.listen(port, () => {
+    console.log(`Servidor está rodando na porta ${port}`);
 });
